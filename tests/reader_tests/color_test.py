46670c57b1359426669cb69fd451a83aa6bb88b9
Toren Darby <torendarby@gmail.com>
2022-12-09 13:48:35 -0800

    refactor: color_test.py

--- tests/reader_tests/color_test.py
+++ tests/reader_tests/color_test.py
@@ -1,21 +1,23 @@
+import pytest
+
 from tests.basicTest import BasicTest
 from reader.color import ColorReader
 
 
-class ColorReaderTest(BasicTest):
-    def run(self):
-        assert ColorReader.read_color("red") == (1, 0, 0)
-        assert ColorReader.read_color("#00ff00") == (0, 1, 0)
-        assert ColorReader.read_color("#000099") == (0, 0, 0.6)
-        assert ColorReader.read_color("#999900") == (0.6, 0.6, 0)
+@pytest.mark.parametrize(
+    "color, expected",
+    [
+        ("red", (1, 0, 0)),
+        ("#00ff00", (0, 1, 0)),
+        ("#000099", (0, 0, 0.6)),
+        ("#999900", (0.6, 0.6, 0)),
+    ],
+)
+def test_color_reader(color, expected):
+    assert ColorReader.read_color(color) == expected
 
-        self.ensureNotAllowed("123", "123 is not a color")
-        self.ensureNotAllowed("text", "text is not a color")
-        self.ensureNotAllowed("]", "empty string is not a color")
 
-    def ensureNotAllowed(self, value, msg):
-        try:
-            ColorReader.read_color(value)
-            assert False, msg
-        except ValueError:
-            1
+@pytest.mark.parametrize("bad_color", ["123", "text", "]"])
+def test_color_reader_rejects_invalid(bad_color):
+    with pytest.raises(ValueError):
+        ColorReader.read_color(bad_color)
