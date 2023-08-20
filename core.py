69f54996c7aa8a8c0ae2f4d06c4dd5180790fff6
Toren Darby <torendarby@gmail.com>
2023-08-19 23:44:51 -0700

    refactor reorder functions again

--- core.py
+++ core.py
@@ -24,8 +24,8 @@ from yaml_parsing import GameSet, RulesDeck
 
 
 def build(image_builder):
-    _save_schema()
-    _copy_yaml_to_site()
+    save_schema()
+    copy_yaml_to_site()
 
     save_dir = Path(try_and_find_save_games_folder())
     yaml_file_to_tts_save(
@@ -33,8 +33,21 @@ def build(image_builder):
     )
 
 
+def save_schema():
+    schema = yaml_parsing.Game.schema_json()
+    with open("data/game_schema.json", "w") as f:
+        f.write(schema)
+
+
+def copy_yaml_to_site():
+    with open("data/input.yaml", "r") as f:
+        input_yaml = f.read()
+    with open("tactics-site/public/input.yaml", "w+") as f:
+        f.write(input_yaml)
+
+
 def yaml_file_to_tts_save(yaml_path: str, save_dir: Path, image_builder: ImageBuilder):
-    game = _load_game_from_yaml_path(yaml_path)
+    game = load_game_from_yaml_path(yaml_path)
     library = game_to_library(game)
 
     tts_dict = asyncio.run(
@@ -49,6 +62,17 @@ def yaml_file_to_tts_save(yaml_path: str, save_dir: Path, image_builder: ImageBu
     print("Built images")
 
 
+def load_game_from_yaml_path(yaml_path: str):
+    try:
+        yaml_content = yaml_parsing.read_yaml_file(yaml_path)
+    except yaml.scanner.ScannerError as e:
+        print(f"Error parsing {yaml_path}\n{e}")
+        return
+
+    game = yaml_parsing.Game.parse_obj(yaml_content)
+    return game
+
+
 def game_to_library(game):
     library = Library(
         tokens=[],
@@ -60,174 +84,16 @@ def game_to_library(game):
 
     sets_bag = Bag(name="Sets", size=3, color=(1.0, 1.0, 1.0))
     for game_set in game.sets:
-        game_set_bag = _make_game_set_bag(game_set)
+        game_set_bag = make_game_set_bag(game_set)
         sets_bag.contained_objects.append(game_set_bag)
     library.bags.append(sets_bag)
 
-    rules_deck = RulesDeck(cards=game.rules)
-    domain_rules_deck = rules_deck.get_tts_obj(set_name="core")
-    library.bags[0].contained_objects.append(domain_rules_deck)
+    add_game_rules_deck(library, game)
 
     return library
 
 
-async def library_to_tts_dict(
-    library: Library,
-    image_builder: ImageBuilder,
-    file_name,
-    config=None,
-):
-    await image_builder.initialize()
-    _setup_pygame()
-
-    tts_dict = _read_template_dict(file_name)
-
-    async with async_playwright() as playwright:
-        await draw_library_assets(
-            playwright=playwright,
-            library=library,
-            config=config,
-            image_builder=image_builder,
-        )
-
-    entities = library.bags
-    tts_dict["ObjectStates"] += [entity.as_dict() for entity in entities]
-    _reposition_set_bag(tts_dict)
-
-    return tts_dict
-
-
-async def draw_library_assets(
-    playwright: Playwright, library: Library, config, image_builder: ImageBuilder
-):
-    await create_browser(playwright)
-
-    coroutines = []
-
-    back_drawer = CardBackDrawer(config)
-
-    deck_names = [deck.name for deck in library.decks]
-    assert len(deck_names) == len(set(deck_names)), "Deck names must be unique"
-
-    for deck in library.decks:
-        for i, card in enumerate(deck.cards):
-            card_drawer = ComplexObjectDrawer(card.object, config)
-            names = [deck.set_name, deck.name, card.object.content.name]
-            image_name = _make_image_name(names)
-            coroutines.append(
-                _save_image_and_set_attribute(
-                    image_builder=image_builder,
-                    drawer=card_drawer,
-                    object_=card,
-                    file_name=image_name,
-                    file_extension="jpg",
-                    attribute_to_set=["image_path"],
-                )
-            )
-
-    for lone_card in library.lone_cards:
-        coroutines.append(
-            _save_image_and_set_attribute(
-                image_builder=image_builder,
-                drawer=LoneCardDrawer(config),
-                object_=lone_card,
-                file_name=lone_card.object.name,
-                file_extension="jpg",
-            )
-        )
-
-    for token in library.tokens:
-        if isinstance(token, ContentToken):
-            coroutines.append(
-                _save_image_and_set_attribute(
-                    image_builder=image_builder,
-                    drawer=back_drawer,  # todo why? Seems to work though.
-                    object_=token,
-                    file_name="token_" + token.name,
-                    file_extension="jpg",
-                    attribute_to_set="image_path",
-                )
-            )
-
-    await asyncio.gather(*coroutines)
-    await close_browser()
-
-
-def save_tts(tts_json: dict, save_dir: Path, file_name: str):
-    path = save_dir / f"TS_{file_name.replace(' ', '_')}.json"
-    with open(path, "w") as outfile:
-        json.dump(tts_json, outfile)
-
-
-def _save_schema():
-    schema = yaml_parsing.Game.schema_json()
-    with open("data/game_schema.json", "w") as f:
-        f.write(schema)
-
-
-def _copy_yaml_to_site():
-    with open("data/input.yaml", "r") as f:
-        input_yaml = f.read()
-    with open("tactics-site/public/input.yaml", "w+") as f:
-        f.write(input_yaml)
-
-
-def _load_game_from_yaml_path(yaml_path: str):
-    try:
-        yaml_content = yaml_parsing.read_yaml_file(yaml_path)
-    except yaml.scanner.ScannerError as e:
-        print(f"Error parsing {yaml_path}\n{e}")
-        return
-
-    game = yaml_parsing.Game.parse_obj(yaml_content)
-    return game
-
-
-def _read_template_dict(file_name: str):
-    # open save template
-    with open("data/template.json", "r") as infile:
-        data = json.load(infile)
-        data["SaveName"] = file_name
-    return data
-
-
-def _make_image_name(names):
-    return "__".join(names)
-
-
-async def _save_image_and_set_attribute(
-    image_builder: ImageBuilder,
-    drawer: BaseDrawer,
-    object_,
-    file_name: str,
-    file_extension: str,
-    attribute_to_set: str | list[str] = None,
-):
-    if attribute_to_set is None:
-        attribute_to_set = ["image_path", "back_image_path"]
-
-    image: Union[Surface, Coroutine[Surface]] = drawer.draw(object_)
-    # if image is promise, resolve
-    if isinstance(image, Coroutine):
-        image = await image
-    path = await image_builder.build(
-        image=image, file_name=file_name, file_extension=file_extension
-    )
-    if isinstance(attribute_to_set, str):
-        attribute_to_set = [attribute_to_set]
-
-    for attribute in attribute_to_set:
-        object_.__setattr__(attribute, path)
-
-
-def _setup_pygame():
-    # setup pygame as drawing library
-    import pygame
-
-    pygame.init()
-
-
-def _make_game_set_bag(game_set: GameSet):
+def make_game_set_bag(game_set: GameSet):
     set_bag = Bag(
         name=game_set.name,
         description=game_set.description,
@@ -264,7 +130,7 @@ def _make_game_set_bag(game_set: GameSet):
     return set_bag
 
 
-def _reposition_set_bag(tts_dict):
+def reposition_set_bag(tts_dict):
     # todo could combine with _make_game_set_bag
     sets_bag = [
         item for item in tts_dict["ObjectStates"] if item["Nickname"] == "Sets"
@@ -280,3 +146,141 @@ def _reposition_set_bag(tts_dict):
         "scaleY": 3.0,
         "scaleZ": 3.0,
     }
+
+
+def add_game_rules_deck(library, game):
+    rules_deck = RulesDeck(cards=game.rules)
+    domain_rules_deck = rules_deck.get_tts_obj(set_name="core")
+    library.bags[0].contained_objects.append(domain_rules_deck)
+
+
+async def library_to_tts_dict(
+    library: Library,
+    image_builder: ImageBuilder,
+    file_name,
+    config=None,
+):
+    await image_builder.initialize()
+    setup_pygame()
+
+    tts_dict = read_template_dict(file_name)
+
+    async with async_playwright() as playwright:
+        await draw_library_assets(
+            playwright=playwright,
+            library=library,
+            config=config,
+            image_builder=image_builder,
+        )
+
+    entities = library.bags
+    tts_dict["ObjectStates"] += [entity.as_dict() for entity in entities]
+    reposition_set_bag(tts_dict)
+
+    return tts_dict
+
+
+def setup_pygame():
+    # setup pygame as drawing library
+    import pygame
+
+    pygame.init()
+
+
+def read_template_dict(file_name: str):
+    # open save template
+    with open("data/template.json", "r") as infile:
+        data = json.load(infile)
+        data["SaveName"] = file_name
+    return data
+
+
+async def draw_library_assets(
+    playwright: Playwright, library: Library, config, image_builder: ImageBuilder
+):
+    await create_browser(playwright)
+
+    coroutines = []
+
+    back_drawer = CardBackDrawer(config)
+
+    deck_names = [deck.name for deck in library.decks]
+    assert len(deck_names) == len(set(deck_names)), "Deck names must be unique"
+
+    for deck in library.decks:
+        for i, card in enumerate(deck.cards):
+            card_drawer = ComplexObjectDrawer(card.object, config)
+            names = [deck.set_name, deck.name, card.object.content.name]
+            image_name = make_image_name(names)
+            coroutines.append(
+                save_image_and_set_attribute(
+                    image_builder=image_builder,
+                    drawer=card_drawer,
+                    object_=card,
+                    file_name=image_name,
+                    file_extension="jpg",
+                    attribute_to_set=["image_path"],
+                )
+            )
+
+    for lone_card in library.lone_cards:
+        coroutines.append(
+            save_image_and_set_attribute(
+                image_builder=image_builder,
+                drawer=LoneCardDrawer(config),
+                object_=lone_card,
+                file_name=lone_card.object.name,
+                file_extension="jpg",
+            )
+        )
+
+    for token in library.tokens:
+        if isinstance(token, ContentToken):
+            coroutines.append(
+                save_image_and_set_attribute(
+                    image_builder=image_builder,
+                    drawer=back_drawer,  # todo why? Seems to work though.
+                    object_=token,
+                    file_name="token_" + token.name,
+                    file_extension="jpg",
+                    attribute_to_set="image_path",
+                )
+            )
+
+    await asyncio.gather(*coroutines)
+    await close_browser()
+
+
+def make_image_name(names):
+    return "__".join(names)
+
+
+async def save_image_and_set_attribute(
+    image_builder: ImageBuilder,
+    drawer: BaseDrawer,
+    object_,
+    file_name: str,
+    file_extension: str,
+    attribute_to_set: str | list[str] = None,
+):
+    if attribute_to_set is None:
+        attribute_to_set = ["image_path", "back_image_path"]
+
+    image: Union[Surface, Coroutine[Surface]] = drawer.draw(object_)
+    # if image is promise, resolve
+    if isinstance(image, Coroutine):
+        image = await image
+    path = await image_builder.build(
+        image=image, file_name=file_name, file_extension=file_extension
+    )
+    if isinstance(attribute_to_set, str):
+        attribute_to_set = [attribute_to_set]
+
+    for attribute in attribute_to_set:
+        object_.__setattr__(attribute, path)
+
+
+def save_tts(tts_json: dict, save_dir: Path, file_name: str):
+    path = save_dir / f"TS_{file_name.replace(' ', '_')}.json"
+    with open(path, "w") as outfile:
+        json.dump(tts_json, outfile)
