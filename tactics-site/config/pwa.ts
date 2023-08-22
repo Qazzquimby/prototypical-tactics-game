62af5395277f8612c30c1543b20cfcf1fcda0f6a
Toren Darby <torendarby@gmail.com>
2023-08-21 23:02:14 -0700

    add jpg to pwa workbox

--- tactics-site/config/pwa.ts
+++ tactics-site/config/pwa.ts
@@ -35,7 +35,7 @@ export const pwa: ModuleOptions = {
     ],
   },
   workbox: {
-    globPatterns: ['**/*.{js,css,html,txt,png,ico,svg}'],
+    globPatterns: ['**/*.{js,css,html,txt,png,ico,svg,jpg,jpeg}'],
     navigateFallbackDenylist: [/^\/api\//],
     navigateFallback: '/',
     cleanupOutdatedCaches: true,
