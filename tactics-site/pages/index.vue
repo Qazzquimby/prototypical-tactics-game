b375d49167737729d87e881f348f92c8331ff9f6
Toren Darby <torendarby@gmail.com>
2023-08-25 12:41:53 -0700

    greatly reduce disgustingness of ui

--- tactics-site/pages/index.vue
+++ tactics-site/pages/index.vue
@@ -15,36 +15,35 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="yamlContent" m-4 font-sans text-gray-700 md:m-6>
-    <div v-for="set of yamlContent.sets" :key="set.name" mb-6 rounded bg-white p-4 shadow-md>
-      <div>
-        <h2 text-primary-700 mb-2 text-xl font-bold>
-          {{ set.name }}
-        </h2>
-        <pre text-gray-600>{{ set.description }}</pre>
-      </div>
+  <div v-if="yamlContent" m-4 font-sans md:m-6>
+    <ICollapsible>
+      <ICollapsibleItem v-for="set of yamlContent.sets" :key="set.name" :title="set.name" accordion rounded bg-gray shadow-md>
+        <div>
+          <pre>{{ set.description }}</pre>
+        </div>
 
-      <div v-for="rule of set.rules" :key="rule.name" my-2 rounded bg-blue-100 p-2>
-        <div flex items-start>
-          <img
-            :src="get_image_path([set.name, `${set.name}_rules`, rule.name])" :alt="rule.name"
-            mx-2 h-32 rounded object-contain
-          >
-          <div>
-            <h3 text-secondary-700 font-bold>
-              {{ rule.name }}
-            </h3>
-            <pre>{{ rule.text }}</pre>
+        <div v-for="rule of set.rules" :key="rule.name" my-2 border border-gray rounded p-2>
+          <div flex items-start>
+            <img
+              :src="get_image_path([set.name, `${set.name}_rules`, rule.name])" :alt="rule.name"
+              mx-2 h-32 rounded object-contain
+            >
+            <div>
+              <h3 text-secondary-700 font-bold>
+                {{ rule.name }}
+              </h3>
+              <pre>{{ rule.text }}</pre>
+            </div>
           </div>
         </div>
-      </div>
 
-      <ICollapsible>
-      <ICollapsibleItem :title="hero.name" accordian v-for="hero of set.heroes" :key="hero.name" my-4 b-2 rounded bg-gray-50 p-2>
-        <Hero :hero="hero" :set="set" />
+        <ICollapsible>
+          <ICollapsibleItem v-for="hero of set.heroes" :key="hero.name" :title="hero.name" accordian rounded bg-gray-50>
+            <Hero :hero="hero" :set="set" />
+          </ICollapsibleItem>
+        </ICollapsible>
       </ICollapsibleItem>
-      </ICollapsible>
-    </div>
+    </ICollapsible>
   </div>
 </template>
 
