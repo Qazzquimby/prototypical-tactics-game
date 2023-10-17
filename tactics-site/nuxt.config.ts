fcc9c1f98678547ee4275e1187960d24aec16ecd
Toren Darby <torendarby@gmail.com>
2023-10-16 22:15:21 -0700

    chore: add firebase

--- tactics-site/nuxt.config.ts
+++ tactics-site/nuxt.config.ts
@@ -1,69 +1,82 @@
-import { pwa } from './config/pwa';
-import { appDescription } from './constants/index';
+import { pwa } from './config/pwa'
+import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
-    modules: [
-        '@inkline/plugin/nuxt',
-        '@vueuse/nuxt',
-        '@unocss/nuxt',
-        '@pinia/nuxt',
-        '@nuxtjs/color-mode',
-        '@vite-pwa/nuxt'
-    ],
-
-    experimental: {
-        // when using generate, payload js assets included in sw precache manifest
-        // but missing on offline, disabling extraction it until fixed
-        payloadExtraction: false,
-        inlineSSRStyles: false,
-        renderJsonPayloads: true,
-        typedPages: true
+  modules: [
+    'nuxt-vuefire',
+    '@inkline/plugin/nuxt',
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@vite-pwa/nuxt',
+  ],
+  vuefire: {
+    config: {
+      apiKey: 'AIzaSyDMMtuzQY8-0TzjMZD9Ll8pi4HMxOOAWLg',
+      authDomain: 'tabletop-teamfight.firebaseapp.com',
+      projectId: 'tabletop-teamfight',
+      storageBucket: 'tabletop-teamfight.appspot.com',
+      messagingSenderId: '831358413965',
+      appId: '1:831358413965:web:282ff4ddbe5046839786c4',
     },
+  },
+  services: {
+    firestore: true,
+  },
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    inlineSSRStyles: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
 
-    css: ['@unocss/reset/tailwind.css'],
+  css: ['@unocss/reset/tailwind.css'],
 
-    colorMode: {
-        classSuffix: ''
+  colorMode: {
+    classSuffix: '',
+  },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
     },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/', '/images/*'],
+      ignore: ['/hi'],
+    },
+  },
 
-    nitro: {
-        esbuild: {
-            options: {
-                target: 'esnext'
-            }
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
         },
-        prerender: {
-            crawlLinks: false,
-            routes: ['/', '/images/*'],
-            ignore: ['/hi']
-        }
+        { name: 'description', content: appDescription },
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: 'black-translucent',
+        },
+      ],
     },
+  },
 
-    app: {
-        head: {
-            viewport: 'width=device-width,initial-scale=1',
-            link: [
-                { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-                { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-                { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
-            ],
-            meta: [
-                {
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1'
-                },
-                { name: 'description', content: appDescription },
-                {
-                    name: 'apple-mobile-web-app-status-bar-style',
-                    content: 'black-translucent'
-                }
-            ]
-        }
-    },
+  pwa,
 
-    pwa,
-
-    devtools: {
-        enabled: true
-    }
-});
+  devtools: {
+    enabled: true,
+  },
+})
